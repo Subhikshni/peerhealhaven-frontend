54fe7bb87ca00a6a5e6424723020f5aa88405213
Subhikshni V S <subhikshni@gmail.com>
2024-05-19 21:33:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import axios from "axios";
+import "./App.css";
+import Profile from "./components/ProfileLogin.js";
+import ProfileSignup from "./components/ProfileSignup.js";
+import Home from "./components/home.js";
+import Home2 from "./components/home2.js";
+import Chatbot from "./components/Chatbot.js";
+import Exercise from "./components/Exercise.js";
+import MoodTracker from "./components/MoodTracker.js";
 
-function App() {
+import Profile2 from "./components/profile2.js";
+
+axios.defaults.baseURL = "https://6c67-52-62-135-165.ngrok-free.app";
+axios.defaults.headers = {
+  "Content-Type": "application/json",
+  "ngork-skip-browser-warning": "69420",
+};
+
+const handleLogin = async (username, password, navigate) => {
+  try {
+    const formData = { username, password };
+    const response = await axios.post("/login", formData);
+    console.log(response.data);
+    if (response.ok) {
+      navigate(`/profile2/${username}`);
+    }
+  } catch (error) {
+    console.error("Error logging in:", error);
+  }
+};
+
+const handleSignup = async (
+  fullname,
+  email,
+  username,
+  password,
+  confirmPassword,
+  navigate
+) => {
+  try {
+    const formData = { fullname, email, username, password, confirmPassword };
+    const response = await axios.post("/signup", formData);
+    console.log(response.data);
+    navigate(`/profile2/${username}`);
+  } catch (error) {
+    console.error("Error submitting form data:", error);
+  }
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App-header">
+        <Routes>
+          <Route
+            path="/profile"
+            element={<Profile handleLogin={handleLogin} />}
+          />
+          <Route
+            path="/profile/signup"
+            element={<ProfileSignup handleSignup={handleSignup} />}
+          />
+          <Route path="/" element={<Home />} />
+          <Route path="/profile2/:username/home2" element={<Home2 />} />
+          <Route
+            path="/profile2/:username/MoodTracker"
+            element={<MoodTracker />}
+          />
+          <Route path="/profile2/:username/exercise" element={<Exercise />} />
+          <Route path="/profile2/:username/chatbot" element={<Chatbot />} />
+          <Route path="/profile2/:username" element={<Profile2 />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
